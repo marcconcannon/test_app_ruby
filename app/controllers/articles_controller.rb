893899415780cc609dad527a6d09202e87e2ccab
Marc Concannon <marcconcannon@gmail.com>
2018-03-06 16:26:33 +0000

    no message

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,25 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+def index
+  @articleS = Article.all
+end
+
+def edit
+  @article = Article.find(params[:id]) #params is a hash.!!!
+  end
+
+  def update
+    @article = Article.find(params[:id]) #params is a hash.!!!
+    if @article.update(article_params)
+      flash[:notice] = "Article was updated"
+      redirect_to article_path(@article)
+    end
+  end
+
+
+
+
   def create
     # render plain: params[:article].inspect
     @article = Article.new(article_params)
@@ -18,6 +37,12 @@ def show
 @article = Article.find(params[:id]) #params is a hash.!!!
 end
 
+def destroy
+@article = Article.find(params[:id]) #params is a hash.!!!
+@article.destroy
+flash[:notice] = 'The article is now gone mo fo'
+redirect_to articles_path
+end
 
 
   private
