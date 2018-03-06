d23c0cf6426f1a75579c17faeed5bc4d4cea32cc
Marc Concannon <marcconcannon@gmail.com>
2018-03-06 16:43:06 +0000

    no message

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,26 +1,29 @@
 class ArticlesController < ApplicationController
+  before_action :set_marcs_article, only:[:edit, :update, :show,:destroy]
+
   def new
     @article = Article.new
   end
 
-def index
-  @articleS = Article.all
-end
-
-def edit
-  @article = Article.find(params[:id]) #params is a hash.!!!
+  def index
+    @articleS = Article.all
   end
 
+  def edit
+    #  @article = Article.find(params[:id]) # params is a hash.!!!
+    #line above no longer needed due to ren bfore command at top of file
+
+    end
+
   def update
-    @article = Article.find(params[:id]) #params is a hash.!!!
+    #  @article = Article.find(params[:id]) # params is a hash.!!!
+    #line above no longer needed due to ren bfore command at top of file
+
     if @article.update(article_params)
-      flash[:notice] = "Article was updated"
+      flash[:notice] = 'Article was updated'
       redirect_to article_path(@article)
     end
   end
-
-
-
 
   def create
     # render plain: params[:article].inspect
@@ -33,19 +36,25 @@ def edit
    end
   end
 
-def show
-@article = Article.find(params[:id]) #params is a hash.!!!
-end
+  def show
+  #  @article = Article.find(params[:id]) # params is a hash.!!!
+  #line above no longer needed due to ren bfore command at top of file
 
-def destroy
-@article = Article.find(params[:id]) #params is a hash.!!!
-@article.destroy
-flash[:notice] = 'The article is now gone mo fo'
-redirect_to articles_path
-end
+  end
 
+  def destroy #
+    #  @article = Article.find(params[:id]) # params is a hash.!!!
+    #line above no longer needed due to ren bfore command at top of file    @article.destroy
+    @article.destroy
+    flash[:notice] = 'The article is now gone mo fo'
+    redirect_to articles_path
+  end
 
   private
+
+  def set_marcs_article
+    @article = Article.find(params[:id])
+  end
 
   def article_params
     params.require(:article).permit(:title, :description)
